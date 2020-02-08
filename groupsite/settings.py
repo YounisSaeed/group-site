f6a468eddbe094762167aca170ab5b90e36e9b0a
YounisSaeed <youniszidan94@gmail.com>
2020-02-09 01:40:19 +0200

    clean

--- groupsite/settings.py
+++ groupsite/settings.py
@@ -73,13 +73,18 @@ WSGI_APPLICATION = 'groupsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'djongo',
+        'NAME': 'groupsite',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
